5b580f81835dd95037168a9eb841170925ebd87a
xiezhimin <xiezm@dachentech.com.cn>
2020-10-27 18:13:54 +0800

    Initial Commit

---
+++ DCMainThreadWatcher/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  DCMainThreadWatcher
+//
+//  Created by xzm on 2020/10/27.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ DCMainThreadWatcher/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  DCMainThreadWatcher
+//
+//  Created by xzm on 2020/10/27.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
